dc4a09ff7ec993664e329154ed764f132247933d
greenjaguarr <marksmit2002@ziggo.nl>
2024-05-03 17:10:41 +0200

    Veel shit veranderd, alles in sudoku utils gemikt en fill begonnen met maken maar het werkt niet en ik weet niet waarom

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,134 +1,37 @@
-import numpy as np
-BOARD = np.zeros(81,dtype='int')
-BLOCK1= np.array([1,2,3,10,11,12,19,20,21])
-BLOCK2= BLOCK1+3
-BLOCK3= BLOCK2+3
-BLOCK4= BLOCK1+27
-BLOCK5= BLOCK4+3
-BLOCK6= BLOCK5+3
-BLOCK7= BLOCK4+27
-BLOCK8= BLOCK7+3
-BLOCK9= BLOCK8+3
-BLOCKS = [BLOCK1,BLOCK2,BLOCK3,BLOCK4,BLOCK5,BLOCK6,BLOCK7,BLOCK8,BLOCK9]
-# BLOCK_to_name = { BLOCK:str(j)for j,BLOCK in enumerate(BLOCKS)}
-# print(BLOCK7)
-def test_BLOCKS():
-    for i in range(1,82):
-        i_is_found = False
-        for BLOCK in BLOCKS:
-            if i in BLOCK:
-                i_is_found = True
-        if i_is_found == False:
-            print("Something went wrong",i)
+from sudoku_utils import Sudoku
 
-# Make functions
-def show_BOARD(local_BOARD=BOARD):
-    '''Properly turns 81 items long list to print'''
-    for i in range(3):
-        #Print3blocks
-        for j in range(3):
-            start= 27*i + 9*j
-            stop = start + 9
-            to_print = local_BOARD[start:stop]
-            print(to_print)
-        if not i==2:
-            print('-----------------------------')
+fill_amount_start = 0
+to_fill = 81
+def fill(fill_amount):
+    if fill_amount == to_fill: #Exit condition
+        print('#We are done')
+        return #Void function
+    index = 81 - (to_fill-fill_amount)
+    possible_numbers = [1,2,3,4,5,6,7,8,9]
+    for num in possible_numbers:
+        if sudoku.is_valid(num,index):
+            sudoku.BOARD[index]=num
+            fill(fill_amount+1)
+        else:
+            pass #Waarom werkt het enigszins
+    #No numbers were valid
+    # print("Hello there")
+    if fill_amount<9:
+        print("whoopsieeee")
+help = True
 
-def test_show_BOARD():
-    BOARD[:5] = [1,2,3,4,5]
-    show_BOARD(BOARD)
+    
+    
 
+sudoku = Sudoku()
+sudoku.fill = fill
 
-def which_BLOCK(i:int)->int:
-    '''This function returns the BLOCK that i is in'''
-    msd=(i-1)//27
-    # lsd = (i-27*msd) // 9 
-    lsd = ((i-1)%9)//3
-    index = 3*msd + lsd + 1
-    #Could also iterate through each block to look if index is there
-    return index
+if help:
+    s = [1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3,7,8,9,1,2,3,4,5,6,2,1,4,3]
+    sudoku.BOARD[:len(s)] = s
+    fill_amount_start = len(s)
+sudoku.show_BOARD()
 
-def test_which_BLOCK():
-    indeces = np.linspace(1,81,81)
-    results = which_BLOCK(indeces)
-    # print(results)
-    # show_BOARD(results)
-    expected_results=[1,1,1,2,2,2,3,3,3,
-                      1,1,1,2,2,2,3,3,3,
-                      1,1,1,2,2,2,3,3,3,
-                      4,4,4,5,5,5,6,6,6,
-                      4,4,4,5,5,5,6,6,6,
-                      4,4,4,5,5,5,6,6,6,
-                      7,7,7,8,8,8,9,9,9,
-                      7,7,7,8,8,8,9,9,9,
-                      7,7,7,8,8,8,9,9,9]
-    for result,expected_result in zip(results,expected_results):
-        if not result==expected_result:
-            print("Test which block failed.")
-    print("Test which block finished")
-def which_row(i:int)->int:
-    return ((i-1) // 9) + 1
+sudoku.fill(fill_amount=fill_amount_start)
 
-def test_which_row():
-    indeces = np.linspace(1,81,81)
-    rows = which_row(indeces)
-    for index,row in zip(indeces,rows):
-        # print(index,row)
-        if not (index - (row-1)*9) in range(10):
-            print(f"Test which row didnt pass because {index} resulted in {row}")
-            pass
-    print("Test which column finished")
-
-def which_column(i:int)->int:
-    return ((i-1)%9) + 1
-
-def test_which_column():
-    indeces = np.linspace(1,81,81)
-    columns = which_column(indeces)
-    for index,column in zip(indeces,columns):
-        # print(index,column, (index-column)/9)
-        if not (index-column)/9 == (index-column)//9:
-            print(f"Test which column didnt pass because {index} resulted in {column}")
-    print("Test which column finished")
-
-def is_column_valid(num,i)->bool:
-    column = which_column(i)
-    indices = np.array([1,10,19,28,37,46,55,64,73]) + column - 1
-    to_check = BOARD[indices]
-    if num in to_check: return False
-    else: return True
-
-def is_row_valid(num,i)->bool:
-    row = which_row(i)
-    indices = (row-1)*9 + np.array([1,2,3,4,5,6,6,7,8,9])
-    to_check = BOARD[indices]
-    if num in to_check: return False
-    else: return True
-
-def is_block_valid(num,i)->bool:
-    block_index = which_BLOCK(i)
-    block = BLOCKS[block_index-1]
-    to_check = BOARD[block]
-    if num in to_check: return False
-    else: return True
-
-
-def is_valid(num:int,i:int,BOARD=BOARD)->bool:
-    '''This function tests is num can be places at place i in BOARD'''
-    row_valid = is_row_valid(num,i)
-    if not row_valid: return False
-    block_valid = is_block_valid(num,i)
-    if not block_valid: return False
-    column_valid = is_column_valid(num,i)
-    if not column_valid: return False
-    return True
-
-
-test_which_BLOCK()
-test_which_column()
-test_which_row()
-
-
-BOARD[:9] = [1,2,3,4,5,6,7,8,9]
-show_BOARD()
-print(is_valid(6,20))
+sudoku.show_BOARD()
---
+++ sudoku_utils.py
@@ -0,0 +1,139 @@
+import numpy as np
+class Sudoku():
+    def __init__(self):
+        self.BOARD = np.zeros(81,dtype='int')
+        self.BLOCK1= np.array([1,2,3,10,11,12,19,20,21]) #We gebruiken index beginnend bij 0
+        self.BLOCK2= self.BLOCK1+3
+        self.BLOCK3= self.BLOCK2+3
+        self.BLOCK4= self.BLOCK1+27
+        self.BLOCK5= self.BLOCK4+3
+        self.BLOCK6= self.BLOCK5+3
+        self.BLOCK7= self.BLOCK4+27
+        self.BLOCK8= self.BLOCK7+3
+        self.BLOCK9= self.BLOCK8+3
+        self.BLOCKS = [self.BLOCK1,self.BLOCK2,self.BLOCK3,self.BLOCK4,self.BLOCK5,self.BLOCK6,self.BLOCK7,self.BLOCK8,self.BLOCK9]
+
+
+# BLOCK_to_name = { BLOCK:str(j)for j,BLOCK in enumerate(BLOCKS)}
+# print(BLOCK7)
+    def test_BLOCKS(self):
+        for i in range(1,82):
+            i_is_found = False
+            for BLOCK in self.BLOCKS:
+                if i in BLOCK:
+                    i_is_found = True
+            if i_is_found == False:
+                print("Something went wrong",i)
+
+    # Make functions
+    def show_BOARD(self):
+        '''Properly turns 81 items long list to print'''
+        for i in range(3):
+            #Print3blocks
+            for j in range(3):
+                start= 27*i + 9*j
+                stop = start + 9
+                to_print = self.BOARD[start:stop]
+                print(to_print)
+            if not i==2:
+                print('-----------------------------')
+            else:
+                print('-----------------------------')
+                print('-----------------------------')
+
+
+    def test_show_BOARD(self):
+        self.BOARD[:5] = [1,2,3,4,5]
+        self.show_BOARD(self.BOARD)
+
+
+    def which_BLOCK(self,i:int)->int:
+        '''This function returns the BLOCK that i is in'''
+        msd=(i)//27
+        # lsd = (i-27*msd) // 9 
+        lsd = ((i)%9)//3
+        index = 3*msd + lsd + 1
+        #Could also iterate through each block to look if index is there
+        return index
+
+    def test_which_BLOCK(self):
+        indeces = np.linspace(0,80,81)
+        results = self.which_BLOCK(indeces)
+        # print(results)
+        # show_BOARD(results)
+        expected_results=[1,1,1,2,2,2,3,3,3,
+                        1,1,1,2,2,2,3,3,3,
+                        1,1,1,2,2,2,3,3,3,
+                        4,4,4,5,5,5,6,6,6,
+                        4,4,4,5,5,5,6,6,6,
+                        4,4,4,5,5,5,6,6,6,
+                        7,7,7,8,8,8,9,9,9,
+                        7,7,7,8,8,8,9,9,9,
+                        7,7,7,8,8,8,9,9,9]
+        for result,expected_result in zip(results,expected_results):
+            if not result==expected_result:
+                print("Test which block failed.")
+        print("Test which block finished")
+    def which_row(self,i:int)->int:
+        return ((i) // 9) + 1
+
+    def test_which_row(self):
+        indeces = np.linspace(1,81,81)
+        rows = self.which_row(indeces)
+        for index,row in zip(indeces,rows):
+            # print(index,row)
+            if not (index - (row-1)*9) in range(10):
+                print(f"Test which row didnt pass because {index} resulted in {row}")
+                pass
+        print("Test which column finished")
+
+    def which_column(self,i:int)->int:
+        return (i%9) + 1
+
+    def test_which_column(self):
+        indeces = np.linspace(1,81,81)
+        columns = self.which_column(indeces)
+        for index,column in zip(indeces,columns):
+            # print(index,column, (index-column)/9)
+            if not (index-column)/9 == (index-column)//9:
+                print(f"Test which column didnt pass because {index} resulted in {column}")
+        print("Test which column finished")
+
+    def is_column_valid(self,num,i)->bool:
+        column = self.which_column(i)
+        indices = np.array([1,10,19,28,37,46,55,64,73]) + column - 2
+        to_check = self.BOARD[indices]
+        if num in to_check: return False
+        else: return True
+
+    def is_row_valid(self,num,i)->bool:
+        row = self.which_row(i)
+        indices = (row-1)*9 + np.array([1,2,3,4,5,6,6,7,8,9]) - 1
+        to_check = self.BOARD[indices]
+        if num in to_check: return False
+        else: return True
+
+    def is_block_valid(self,num,i)->bool:
+        block_index = self.which_BLOCK(i)
+        block = self.BLOCKS[block_index-1]
+        to_check = self.BOARD[block-1]
+        if num in to_check: return False
+        else: return True
+
+
+    def is_valid(self,num:int,i:int)->bool:
+        '''This function tests is num can be places at place i in BOARD'''
+        row_valid = self.is_row_valid(num,i)
+        if not row_valid: return False
+        block_valid = self.is_block_valid(num,i)
+        if not block_valid: return False
+        column_valid = self.is_column_valid(num,i)
+        if not column_valid: return False
+        return True
+
+
+if __name__ == "__main__":
+    sudoku = Sudoku()
+    sudoku.test_which_BLOCK()
+    sudoku.test_which_row()
+    sudoku.test_which_column()
